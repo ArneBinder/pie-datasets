af821dbc3970457d0bbbc5d5a9e764ef8ab39511
Arne Binder <arne.binder@dfki.de>
2023-11-25 02:10:20 +0100

    add abstrct dataset

---
+++ dataset_builders/pie/abstrct/README.md
@@ -0,0 +1,23 @@
+# PIE Dataset Card for "abstrct"
+
+This is a [PyTorch-IE](https://github.com/ChristophAlt/pytorch-ie) wrapper for the AbstRCT dataset.
+
+TODO: Since there is no respective HF dataset card, we should all respective information here.
+
+TODO: Shortly reference the PIE-Brat dataset card.
+
+## Data Schema
+
+TODO
+
+See [here](https://github.com/ChristophAlt/pytorch-ie/blob/main/src/pytorch_ie/annotations.py) for the remaining annotation type definitions.
+
+## Document Converters
+
+The dataset provides document converters for the following target document types:
+
+- `pytorch_ie.documents.TextDocumentWithLabeledSpansAndBinaryRelations`
+  - TODO
+
+See [here](https://github.com/ChristophAlt/pytorch-ie/blob/main/src/pytorch_ie/documents.py) for the document type
+definitions.
---
+++ dataset_builders/pie/abstrct/abstrct.py
@@ -0,0 +1,41 @@
+from pytorch_ie.documents import (
+    TextDocumentWithLabeledSpansAndBinaryRelations,
+)
+
+from pie_datasets.builders import BratBuilder
+from pie_datasets.core.dataset import DocumentConvertersType
+
+URL = "https://gitlab.com/tomaye/abstrct/-/archive/master/abstrct-master.zip"
+SPLIT_PATHS = {
+    "neoplasm_train": "abstrct-master/AbstRCT_corpus/data/train/neoplasm_train",
+    "neoplasm_dev": "abstrct-master/AbstRCT_corpus/data/dev/neoplasm_dev",
+    "neoplasm_test": "abstrct-master/AbstRCT_corpus/data/test/neoplasm_test",
+    "glaucoma_test": "abstrct-master/AbstRCT_corpus/data/test/glaucoma_test",
+    "mixed_test": "abstrct-master/AbstRCT_corpus/data/test/mixed_test",
+}
+
+
+class AbstRCT(BratBuilder):
+
+    BASE_DATASET_PATH = "DFKI-SLT/brat"
+    BASE_DATASET_REVISION = "052163d34b4429d81003981bc10674cef54aa0b8"
+
+    # we need to add None to the list of dataset variants to support the default dataset variant
+    BASE_BUILDER_KWARGS_DICT = {
+        dataset_variant: {"url": URL, "split_paths": SPLIT_PATHS}
+        for dataset_variant in ["default", "merge_fragmented_spans", None]
+    }
+
+    @property
+    def document_converters(self) -> DocumentConvertersType:
+        if self.config.name == "default":
+            return {}
+        elif self.config.name == "merge_fragmented_spans":
+            return {
+                TextDocumentWithLabeledSpansAndBinaryRelations: {
+                    "spans": "labeled_spans",
+                    "relations": "binary_relations",
+                },
+            }
+        else:
+            raise ValueError(f"Unknown dataset variant: {self.config.name}")
