eb2264467946d86ea3bb6c634db76a11d5451886
Arne Binder <arne.binder@dfki.de>
2023-11-26 22:18:12 +0100

    set BASE_DATASET_PATH and BASE_DATASET_REVISION directly in brat dataset script

--- dataset_builders/pie/brat/brat.py
+++ dataset_builders/pie/brat/brat.py
@@ -2,4 +2,5 @@ from pie_datasets.builders import BratBuilder
 
 
 class Brat(BratBuilder):
-    pass
+    BASE_DATASET_PATH = "DFKI-SLT/brat"
+    BASE_DATASET_REVISION = "052163d34b4429d81003981bc10674cef54aa0b8"
