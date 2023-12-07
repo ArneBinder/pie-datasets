c105ee0451def6076cbb0a146f9bcda2018c963f
Ruangrin L <88072261+idalr@users.noreply.github.com>
2023-12-07 16:39:42 +0100

    updated `BASE_DATASET_REVISION`

--- dataset_builders/pie/abstrct/abstrct.py
+++ dataset_builders/pie/abstrct/abstrct.py
@@ -15,7 +15,7 @@ SPLIT_PATHS = {
 
 class AbstRCT(BratBuilder):
     BASE_DATASET_PATH = "DFKI-SLT/brat"
-    BASE_DATASET_REVISION = "052163d34b4429d81003981bc10674cef54aa0b8"
+    BASE_DATASET_REVISION = "844de61e8a00dc6a93fc29dc185f6e617131fbf1"
 
     # we need to add None to the list of dataset variants to support the default dataset variant
     BASE_BUILDER_KWARGS_DICT = {
