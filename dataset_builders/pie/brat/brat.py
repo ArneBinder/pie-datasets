ec14a3d408ae29dd47b6b293dfcb5fb0b4ea6410
Arne Binder <arne.binder@dfki.de>
2023-12-07 16:56:21 +0100

    update brat revision to version with fix for encoding for windows

--- dataset_builders/pie/brat/brat.py
+++ dataset_builders/pie/brat/brat.py
@@ -3,4 +3,4 @@ from pie_datasets.builders import BratBuilder
 
 class Brat(BratBuilder):
     BASE_DATASET_PATH = "DFKI-SLT/brat"
-    BASE_DATASET_REVISION = "052163d34b4429d81003981bc10674cef54aa0b8"
+    BASE_DATASET_REVISION = "844de61e8a00dc6a93fc29dc185f6e617131fbf1"
