4097196944a73addca49c98c1b9acc942caebc1b
Arne Binder <arne.binder@dfki.de>
2023-12-09 23:10:39 +0100

    modernize dataset script

--- dataset_builders/pie/imdb/imdb.py
+++ dataset_builders/pie/imdb/imdb.py
@@ -1,28 +1,42 @@
 from dataclasses import dataclass
+from typing import Any, Dict
 
 import datasets
 from pytorch_ie.annotations import Label
-from pytorch_ie.core import AnnotationList, annotation_field
-from pytorch_ie.documents import TextBasedDocument
+from pytorch_ie.documents import TextDocumentWithLabel
 
 from pie_datasets import GeneratorBasedBuilder
 
 
-class ImdbConfig(datasets.BuilderConfig):
-    """BuilderConfig for IMDB."""
-
-    def __init__(self, **kwargs):
-        """BuilderConfig for IMDB.
-
-        Args:
-          **kwargs: keyword arguments forwarded to super.
-        """
-        super().__init__(**kwargs)
-
-
 @dataclass
-class ImdbDocument(TextBasedDocument):
-    label: AnnotationList[Label] = annotation_field()
+class ImdbDocument(TextDocumentWithLabel):
+    pass
+
+
+def example_to_document(example: Dict[str, Any], labels: datasets.ClassLabel) -> ImdbDocument:
+    text = example["text"]
+    document = ImdbDocument(text=text)
+    label_id = example["label"]
+    if label_id < 0:
+        return document
+
+    label = labels.int2str(label_id)
+    label_annotation = Label(label=label)
+    document.label.append(label_annotation)
+
+    return document
+
+
+def document_to_example(document: ImdbDocument, labels: datasets.ClassLabel) -> Dict[str, Any]:
+    if len(document.label) > 0:
+        label_id = labels.str2int(document.label[0].label)
+    else:
+        label_id = -1
+
+    return {
+        "text": document.text,
+        "label": label_id,
+    }
 
 
 class Imdb(GeneratorBasedBuilder):
@@ -32,25 +46,17 @@ class Imdb(GeneratorBasedBuilder):
     BASE_DATASET_REVISION = "9c6ede893febf99215a29cc7b72992bb1138b06b"
 
     BUILDER_CONFIGS = [
-        ImdbConfig(
+        datasets.BuilderConfig(
             name="plain_text",
             version=datasets.Version("1.0.0"),
             description="IMDB sentiment classification dataset",
         ),
     ]
 
-    def _generate_document_kwargs(self, dataset):
+    DOCUMENT_CONVERTERS = {TextDocumentWithLabel: {}}
+
+    def _generate_document_kwargs(self, dataset) -> Dict[str, Any]:
         return {"labels": dataset.features["label"]}
 
-    def _generate_document(self, example, labels: datasets.ClassLabel):
-        text = example["text"]
-        document = ImdbDocument(text=text)
-        label_id = example["label"]
-        if label_id < 0:
-            return document
-
-        label = labels.int2str(label_id)
-        label_annotation = Label(label=label)
-        document.label.append(label_annotation)
-
-        return document
+    def _generate_document(self, example, **kwargs) -> ImdbDocument:
+        return example_to_document(example, **kwargs)
