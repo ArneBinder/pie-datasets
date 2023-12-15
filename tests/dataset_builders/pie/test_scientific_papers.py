7fea36ee1ce0433bf03b09d1e412f46015c45521
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-12-15 10:45:46 +0530

    breakdown ScientificPapersDocument and test with expected data

--- tests/dataset_builders/pie/test_scientific_papers.py
+++ tests/dataset_builders/pie/test_scientific_papers.py
@@ -14,7 +14,7 @@ HF_DATASET_PATH = BUILDER_CLASS.BASE_DATASET_PATH
 PIE_DATASET_PATH = PIE_BASE_PATH / DATASET_NAME
 
 
-@pytest.fixture(scope="module", params=["arxiv", "pubmed"])
+@pytest.fixture(scope="module", params=[config.name for config in BUILDER_CLASS.BUILDER_CONFIGS])
 def dataset_variant(request):
     return request.param
 
@@ -32,20 +32,37 @@ def hf_example(hf_dataset):
 
 
 @pytest.fixture(scope="module")
+def expected_output(dataset_variant):
+    results = {
+        "arxiv": {
+            "article": "additive models @xcite provide an important family of models for semiparametric regression or class",
+            "abstract": " additive models play an important role in semiparametric statistics . \n this paper gives learning r",
+            "section_names": "introduction\nmain results on learning rates\ncomparison of learning rates",
+        },
+        "pubmed": {
+            "article": "a recent systematic analysis showed that in 2011 , 314 ( 296 - 331 ) million children younger than 5",
+            "abstract": " background : the present study was carried out to assess the effects of community nutrition interve",
+            "section_names": "INTRODUCTION\nMATERIALS AND METHODS\nParticipants\nInstruments\nProcedure\nFirst step\nSecond step\nThird s",
+        },
+    }
+    return results[dataset_variant]
+
+
+@pytest.fixture(scope="module")
 def generate_document_kwargs(hf_dataset, dataset_variant):
-    return BUILDER_CLASS(config_name=dataset_variant)._generate_document_kwargs(hf_dataset)
+    return BUILDER_CLASS(config_name=dataset_variant)._generate_document_kwargs(hf_dataset) or {}
 
 
 def test_hf_dataset(hf_dataset):
     assert hf_dataset is not None
 
 
-def test_hf_example(hf_example):
+def test_hf_example(hf_example, expected_output):
     assert hf_example is not None
     assert isinstance(hf_example, dict)
-    assert "article" in hf_example
-    assert "abstract" in hf_example
-    assert "section_names" in hf_example
+    assert hf_example["article"].startswith(expected_output["article"])
+    assert hf_example["abstract"].startswith(expected_output["abstract"])
+    assert hf_example["section_names"].startswith(expected_output["section_names"])
 
 
 def test_generate_document_kwargs(hf_dataset, generate_document_kwargs):
@@ -53,7 +70,7 @@ def test_generate_document_kwargs(hf_dataset, generate_document_kwargs):
     assert isinstance(generate_document_kwargs, dict)
 
 
-def test_generate_document(hf_example, generate_document_kwargs, dataset_variant):
+def test_generate_document(hf_example, generate_document_kwargs, dataset_variant, expected_output):
     doc = BUILDER_CLASS(config_name=dataset_variant)._generate_document(
         hf_example, **generate_document_kwargs
     )
@@ -63,12 +80,13 @@ def test_generate_document(hf_example, generate_document_kwargs, dataset_variant
     assert doc.abstract is not None
     assert doc.section_names is not None
 
-    assert doc.text == hf_example["article"]
-    assert doc.abstract == hf_example["abstract"]
-    assert doc.section_names == hf_example["section_names"].split("\n")
+    assert doc.text.startswith(expected_output["article"])
+    assert doc.abstract[0].text.startswith(expected_output["abstract"])
+    str_section_names = "\n".join([section_name.text for section_name in doc.section_names])
+    assert str_section_names.startswith(expected_output["section_names"])
 
 
-def test_generate_example(hf_example, generate_document_kwargs, dataset_variant):
+def test_generate_example(hf_example, generate_document_kwargs, dataset_variant, expected_output):
     doc = BUILDER_CLASS(config_name=dataset_variant)._generate_document(
         hf_example, **generate_document_kwargs
     )
@@ -76,6 +94,6 @@ def test_generate_example(hf_example, generate_document_kwargs, dataset_variant)
     assert example is not None
     assert isinstance(example, dict)
 
-    assert example["article"] == doc.text
-    assert example["abstract"] == doc.abstract
-    assert example["section_names"] == "\n".join(doc.section_names)
+    assert example["article"].startswith(expected_output["article"])
+    assert example["abstract"].startswith(expected_output["abstract"])
+    assert example["section_names"].startswith(expected_output["section_names"])
