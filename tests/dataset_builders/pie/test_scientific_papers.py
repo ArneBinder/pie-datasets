0ac41ec3909be90ee2bcd2ca154f3e2980e465e6
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-12-15 11:43:24 +0530

    rearrange fixtures and add tests for pie_datasets load_dataset

--- tests/dataset_builders/pie/test_scientific_papers.py
+++ tests/dataset_builders/pie/test_scientific_papers.py
@@ -2,6 +2,7 @@ import pytest
 from datasets import disable_caching, load_dataset
 
 from dataset_builders.pie.scientific_papers.scientific_papers import ScientificPapers
+from pie_datasets import load_dataset as load_pie_dataset
 from tests.dataset_builders.common import PIE_BASE_PATH
 
 disable_caching()
@@ -27,8 +28,22 @@ def hf_dataset(dataset_variant):
 
 
 @pytest.fixture(scope="module")
+def pie_dataset(dataset_variant):
+    dataset = load_pie_dataset(
+        str(PIE_DATASET_PATH), dataset_variant, split="train", streaming=True
+    )
+    dataset_head = dataset.take(STREAM_SIZE)
+    return list(dataset_head)
+
+
+@pytest.fixture(scope="module")
 def hf_example(hf_dataset):
     return hf_dataset[0]
+
+
+@pytest.fixture(scope="module")
+def pie_example(pie_dataset):
+    return pie_dataset[0]
 
 
 @pytest.fixture(scope="module")
@@ -53,8 +68,26 @@ def generate_document_kwargs(hf_dataset, dataset_variant):
     return BUILDER_CLASS(config_name=dataset_variant)._generate_document_kwargs(hf_dataset) or {}
 
 
+@pytest.fixture(scope="module")
+def generated_document(hf_example, generate_document_kwargs, dataset_variant):
+    doc = BUILDER_CLASS(config_name=dataset_variant)._generate_document(
+        hf_example, **generate_document_kwargs
+    )
+    return doc
+
+
+@pytest.fixture(scope="module")
+def generated_example(generated_document, dataset_variant):
+    example = BUILDER_CLASS(config_name=dataset_variant)._generate_example(generated_document)
+    return example
+
+
 def test_hf_dataset(hf_dataset):
     assert hf_dataset is not None
+
+
+def test_pie_dataset(pie_dataset):
+    assert pie_dataset is not None
 
 
 def test_hf_example(hf_example, expected_output):
@@ -65,35 +98,50 @@ def test_hf_example(hf_example, expected_output):
     assert hf_example["section_names"].startswith(expected_output["section_names"])
 
 
+def test_pie_example(pie_example, expected_output):
+    assert pie_example is not None
+    assert pie_example.text.startswith(expected_output["article"])
+    assert pie_example.abstract[0].text.startswith(expected_output["abstract"])
+    str_section_names = "\n".join(
+        [section_name.text for section_name in pie_example.section_names]
+    )
+    assert str_section_names.startswith(expected_output["section_names"])
+
+
 def test_generate_document_kwargs(hf_dataset, generate_document_kwargs):
     assert generate_document_kwargs is not None
     assert isinstance(generate_document_kwargs, dict)
 
 
-def test_generate_document(hf_example, generate_document_kwargs, dataset_variant, expected_output):
-    doc = BUILDER_CLASS(config_name=dataset_variant)._generate_document(
-        hf_example, **generate_document_kwargs
-    )
-    assert doc is not None
-    assert isinstance(doc, DOCUMENT_TYPE)
-    assert doc.text is not None
-    assert doc.abstract is not None
-    assert doc.section_names is not None
+def test_generate_document(generated_document, expected_output):
+    assert generated_document is not None
+    assert isinstance(generated_document, DOCUMENT_TYPE)
+    assert generated_document.text is not None
+    assert generated_document.abstract is not None
+    assert generated_document.section_names is not None
 
-    assert doc.text.startswith(expected_output["article"])
-    assert doc.abstract[0].text.startswith(expected_output["abstract"])
-    str_section_names = "\n".join([section_name.text for section_name in doc.section_names])
+    assert generated_document.text.startswith(expected_output["article"])
+    assert generated_document.abstract[0].text.startswith(expected_output["abstract"])
+    str_section_names = "\n".join(
+        [section_name.text for section_name in generated_document.section_names]
+    )
     assert str_section_names.startswith(expected_output["section_names"])
 
 
-def test_generate_example(hf_example, generate_document_kwargs, dataset_variant, expected_output):
-    doc = BUILDER_CLASS(config_name=dataset_variant)._generate_document(
-        hf_example, **generate_document_kwargs
-    )
-    example = BUILDER_CLASS(config_name=dataset_variant)._generate_example(doc)
-    assert example is not None
-    assert isinstance(example, dict)
+def test_generate_example(generated_example, expected_output):
+    assert generated_example is not None
+    assert isinstance(generated_example, dict)
 
-    assert example["article"].startswith(expected_output["article"])
-    assert example["abstract"].startswith(expected_output["abstract"])
-    assert example["section_names"].startswith(expected_output["section_names"])
+    assert generated_example["article"].startswith(expected_output["article"])
+    assert generated_example["abstract"].startswith(expected_output["abstract"])
+    assert generated_example["section_names"].startswith(expected_output["section_names"])
+
+
+def test_compare_document_and_generated_document(generated_document, pie_example):
+    assert generated_document.text == pie_example.text
+    assert generated_document.abstract[0].text == pie_example.abstract[0].text
+    generated_section_names = [
+        section_name.text for section_name in generated_document.section_names
+    ]
+    pie_section_names = [section_name.text for section_name in pie_example.section_names]
+    assert generated_section_names == pie_section_names
