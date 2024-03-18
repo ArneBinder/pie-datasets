6e355f53fd07e054a186434fceec30b2c9d021b2
Arne Binder <arne.binder@dfki.de>
2024-03-18 18:46:33 +0100

    rearrange and preparation

--- tests/dataset_builders/pie/test_imdb.py
+++ tests/dataset_builders/pie/test_imdb.py
@@ -1,9 +1,10 @@
+import datasets
 import pytest
 from datasets import disable_caching, load_dataset
 from pytorch_ie.core import Document
 
 from dataset_builders.pie.imdb.imdb import Imdb
-from pie_datasets import DatasetDict
+from pie_datasets import Dataset
 from tests.dataset_builders.common import PIE_BASE_PATH
 
 disable_caching()
@@ -14,6 +15,10 @@ DOCUMENT_TYPE = BUILDER_CLASS.DOCUMENT_TYPE
 SPLIT_SIZES = {"test": 25000, "train": 25000, "unsupervised": 50000}
 HF_DATASET_PATH = BUILDER_CLASS.BASE_DATASET_PATH
 PIE_DATASET_PATH = PIE_BASE_PATH / DATASET_NAME
+
+# fast testing parameters
+SPLIT = "train"
+STREAM_SIZE = 3
 
 
 @pytest.fixture(scope="module", params=list(BUILDER_CLASS.DOCUMENT_CONVERTERS))
@@ -27,18 +32,18 @@ def split(request):
 
 
 @pytest.fixture(scope="module")
-def hf_dataset():
-    return load_dataset(str(HF_DATASET_PATH))
+def hf_dataset(split) -> datasets.Dataset:
+    return load_dataset(str(HF_DATASET_PATH), split=split)
 
 
-def test_hf_dataset(hf_dataset):
+def test_hf_dataset(hf_dataset, split):
     assert hf_dataset is not None
-    assert {name: len(ds) for name, ds in hf_dataset.items()} == SPLIT_SIZES
+    assert len(hf_dataset) == SPLIT_SIZES[split]
 
 
 @pytest.fixture(scope="module")
-def hf_example(hf_dataset, split):
-    return hf_dataset[split][0]
+def hf_example(hf_dataset):
+    return hf_dataset[0]
 
 
 def test_hf_example(hf_example, split):
@@ -63,25 +68,31 @@ def test_hf_example(hf_example, split):
 
 
 @pytest.fixture(scope="module")
-def generate_document_kwargs(hf_dataset, split) -> dict:
-    return BUILDER_CLASS()._generate_document_kwargs(hf_dataset[split]) or {}
+def builder() -> BUILDER_CLASS:
+    return BUILDER_CLASS()
 
 
 @pytest.fixture(scope="module")
-def generate_example_kwargs(hf_dataset, split) -> dict:
-    return BUILDER_CLASS()._generate_example_kwargs(hf_dataset[split]) or {}
+def generate_document_kwargs(hf_dataset, builder) -> dict:
+    return builder._generate_document_kwargs(hf_dataset) or {}
 
 
 @pytest.fixture(scope="module")
-def generated_document(hf_example, generate_document_kwargs) -> DOCUMENT_TYPE:
-    return BUILDER_CLASS()._generate_document(hf_example, **generate_document_kwargs)
+def generate_example_kwargs(hf_dataset, builder) -> dict:
+    return builder._generate_example_kwargs(hf_dataset) or {}
+
+
+@pytest.fixture(scope="module")
+def generated_document(hf_example, generate_document_kwargs, builder) -> DOCUMENT_TYPE:
+    return builder._generate_document(hf_example, **generate_document_kwargs)
 
 
 def test_generated_document(generated_document, split):
     assert isinstance(generated_document, DOCUMENT_TYPE)
     if split == "train":
         assert generated_document.text.startswith(
-            "I rented I AM CURIOUS-YELLOW from my video store because of all the controversy that surrounded it when it was first released in 1967."
+            "I rented I AM CURIOUS-YELLOW from my video store because of all the "
+            "controversy that surrounded it when it was first released in 1967."
         )
         assert len(generated_document.text) == 1640
         assert generated_document.label[0].label == "neg"
@@ -99,40 +110,36 @@ def test_generated_document(generated_document, split):
         raise ValueError(f"Unknown split: {split}")
 
 
-@pytest.fixture(scope="module")
-def hf_example_back(generated_document, generate_example_kwargs):
-    return BUILDER_CLASS()._generate_example(
+def test_example_to_document_and_back(hf_example, generated_document, generate_example_kwargs):
+    hf_example_back = BUILDER_CLASS()._generate_example(
         document=generated_document, **generate_example_kwargs
     )
-
-
-def test_example_to_document_and_back(hf_example, hf_example_back):
     assert hf_example_back == hf_example
 
 
 @pytest.mark.slow
 def test_example_to_document_and_back_all(
-    hf_dataset, generate_document_kwargs, generate_example_kwargs, split
+    hf_dataset, generate_document_kwargs, generate_example_kwargs, builder
 ):
-    for hf_ex in hf_dataset[split]:
-        doc = BUILDER_CLASS()._generate_document(example=hf_ex, **generate_document_kwargs)
-        hf_ex_back = BUILDER_CLASS()._generate_example(document=doc, **generate_example_kwargs)
+    for hf_ex in hf_dataset:
+        doc = builder._generate_document(example=hf_ex, **generate_document_kwargs)
+        hf_ex_back = builder._generate_example(document=doc, **generate_example_kwargs)
         assert hf_ex_back == hf_ex
 
 
 @pytest.fixture(scope="module")
-def dataset() -> DatasetDict:
-    return DatasetDict.load_dataset(str(PIE_DATASET_PATH))
+def pie_dataset(split) -> Dataset:
+    return load_dataset(str(PIE_DATASET_PATH), split=split)
 
 
-def test_pie_dataset(dataset):
-    assert dataset is not None
-    assert {name: len(ds) for name, ds in dataset.items()} == SPLIT_SIZES
+def test_pie_dataset(pie_dataset, split):
+    assert pie_dataset is not None
+    assert len(pie_dataset) == SPLIT_SIZES[split]
 
 
 @pytest.fixture(scope="module")
-def document(dataset, split) -> DOCUMENT_TYPE:
-    doc = dataset[split][0]
+def document(pie_dataset) -> DOCUMENT_TYPE:
+    doc = pie_dataset[0]
     # we can not assert the real document type because it may come from a dataset loading script
     # downloaded to a temporary directory and thus have a different type object, although it is
     # semantically the same
@@ -145,16 +152,10 @@ def test_compare_document_and_generated_document(document, generated_document):
     assert document == generated_document
 
 
-@pytest.fixture(scope="module")
-def dataset_with_extractive_qa_documents(dataset, converted_document_type) -> DatasetDict:
-    return dataset.to_document_type(converted_document_type)
-
-
-def test_dataset_with_extractive_qa_documents(
-    dataset_with_extractive_qa_documents, document, split, converted_document_type
-):
-    assert dataset_with_extractive_qa_documents is not None
-    doc = dataset_with_extractive_qa_documents[split][0]
-    assert isinstance(doc, converted_document_type)
+def test_dataset_with_converted_documents(pie_dataset, document, converted_document_type):
+    dataset_with_converted_documents = pie_dataset.to_document_type(converted_document_type)
+    assert dataset_with_converted_documents is not None
+    doc_converted = dataset_with_converted_documents[0]
+    assert isinstance(doc_converted, converted_document_type)
     doc_casted = document.as_type(converted_document_type)
-    assert doc == doc_casted
+    assert doc_converted == doc_casted
