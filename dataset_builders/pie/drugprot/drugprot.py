d563008d700da0ade4c9be0c240b129a905999c4
rainbowrivey <rainbowrivey@hotmail.com>
2024-04-01 06:19:14 +0200

    both dataset types now supported + converters work

--- dataset_builders/pie/drugprot/drugprot.py
+++ dataset_builders/pie/drugprot/drugprot.py
@@ -1,20 +1,129 @@
 from dataclasses import dataclass
+from typing import Any, Dict, Optional
 
 import datasets
-from pytorch_ie.annotations import BinaryRelation, LabeledSpan
-from pytorch_ie.core import AnnotationList, annotation_field
-from pytorch_ie.documents import TextDocumentWithLabeledSpansAndBinaryRelations
+from pytorch_ie.annotations import BinaryRelation, Label, LabeledSpan
+from pytorch_ie.core import AnnotationLayer, annotation_field
+from pytorch_ie.documents import (
+    TextBasedDocument,
+    TextDocumentWithLabeledSpansAndBinaryRelations,
+)
 
 from pie_datasets import GeneratorBasedBuilder
 
 
 @dataclass
-class DrugprotDocument(TextDocumentWithLabeledSpansAndBinaryRelations):
-    pass
+class DrugprotDocument(TextBasedDocument):
+    title: Optional[str] = None
+    abstract: Optional[str] = None
+    entities: AnnotationLayer[LabeledSpan] = annotation_field(target="text")
+    relations: AnnotationLayer[BinaryRelation] = annotation_field(target="entities")
+
+
+@dataclass
+class DrugprotBigbioDocument(TextBasedDocument):
+    passages: AnnotationLayer[LabeledSpan] = annotation_field(target="text")
+    entities: AnnotationLayer[LabeledSpan] = annotation_field(target="text")
+    relations: AnnotationLayer[BinaryRelation] = annotation_field(target="entities")
+
+
+def example2drugprot(example: Dict[str, Any]) -> DrugprotDocument:
+    metadata = {"entity_id": []}
+    id2start = {}
+    for entity in example["entities"]:
+        id2start[entity["id"]] = entity["offset"][0]
+
+    document = DrugprotDocument(
+        text=example["text"],
+        title=example["title"],
+        abstract=example["abstract"],
+        id=example["document_id"],
+        metadata=metadata,
+    )
+    # We sort labels and relation to always have an deterministic order for testing purposes.
+    for span in sorted(example["entities"], key=lambda span: span["offset"][0]):
+        labeled_span = LabeledSpan(
+            start=span["offset"][0],
+            end=span["offset"][1],
+            label=span["type"],
+        )
+        document.entities.append(labeled_span)
+        document.metadata["entity_id"].append(span["id"])
+    for relation in sorted(example["relations"], key=lambda relation: relation["id"]):
+        document.relations.append(
+            BinaryRelation(
+                head=[
+                    span
+                    for span in document.entities
+                    if span.start == id2start[relation["arg1_id"]]
+                ][0],
+                tail=[
+                    span
+                    for span in document.entities
+                    if span.start == id2start[relation["arg2_id"]]
+                ][0],
+                label=relation["type"],
+            )
+        )
+    return document
+
+
+def example2drugprot_bigbio(example: Dict[str, Any]) -> DrugprotBigbioDocument:
+    text = " ".join([" ".join(passage["text"]) for passage in example["passages"]])
+    doc_id = example["document_id"]
+    metadata = {"entity_id": []}
+
+    id2start = {}
+    for entity in example["entities"]:
+        id2start[entity["id"]] = entity["offsets"][0][0]
+
+    document = DrugprotBigbioDocument(
+        text=text,
+        id=doc_id,
+        metadata=metadata,
+    )
+    for passage in example["passages"]:
+        document.passages.append(
+            LabeledSpan(
+                start=passage["offsets"][0][0],
+                end=passage["offsets"][0][1],
+                label=passage["type"],
+            )
+        )
+    # We sort labels and relation to always have an deterministic order for testing purposes.
+    for span in sorted(example["entities"], key=lambda span: span["offsets"][0][0]):
+        document.entities.append(
+            LabeledSpan(
+                start=span["offsets"][0][0],
+                end=span["offsets"][0][1],
+                label=span["type"],
+            )
+        )
+        document.metadata["entity_id"].append(span["id"])
+    for relation in sorted(example["relations"], key=lambda relation: relation["id"]):
+        document.relations.append(
+            BinaryRelation(
+                head=[
+                    span
+                    for span in document.entities
+                    if span.start == id2start[relation["arg1_id"]]
+                ][0],
+                tail=[
+                    span
+                    for span in document.entities
+                    if span.start == id2start[relation["arg2_id"]]
+                ][0],
+                label=relation["type"],
+            )
+        )
+    return document
 
 
 class Drugprot(GeneratorBasedBuilder):
-    DOCUMENT_TYPE = DrugprotDocument
+    DOCUMENT_TYPES = {
+        "drugprot_source": DrugprotDocument,
+        "drugprot_bigbio_kb": DrugprotBigbioDocument,
+    }
 
     BASE_DATASET_PATH = "bigbio/drugprot"
     BASE_DATASET_REVISION = "38ff03d68347aaf694e598c50cb164191f50f61c"
@@ -25,55 +134,35 @@ class Drugprot(GeneratorBasedBuilder):
             version=datasets.Version("1.0.2"),
             description="DrugProt source schema",
         ),
-        # datasets.BuilderConfig(
-        #     name="drugprot_bigbio_kb", version=datasets.Version("1.0.0"), description="DrugProt BigBio schema"
-        # ), # not ready yet
+        datasets.BuilderConfig(
+            name="drugprot_bigbio_kb",
+            version=datasets.Version("1.0.0"),
+            description="DrugProt BigBio schema",
+        ),
     ]
+    DOCUMENT_CONVERTERS = {
+        TextDocumentWithLabeledSpansAndBinaryRelations: {
+            "entities": "labeled_spans",
+            "relations": "binary_relations",
+        }
+    }
+    # @property
+    # def document_converters(self):
+    #     if self.config.name in ("drugprot_source", "drugprot_bigbio_kb"):
+    #         return {
+    #             TextDocumentWithLabeledSpansAndBinaryRelations: {
+    #                 "entities": "labeled_spans",
+    #                 "relations": "binary_relations",
+    #             }
+    #         }
+    #     else:
+    #         raise ValueError(f"Unknown dataset name: {self.config.name}")
 
-    # DOCUMENT_CONVERTERS = {
-    #     TextDocumentWithLabeledSpans: {
-    #         # just rename the layer
-    #         "entities": "labeled_spans",
-    #     }
-    # }
-
-    def _generate_document_kwargs(self, dataset):
-        return {}
-
-    def _generate_document(self, example):
-        text = example["text"]
-        doc_id = example["document_id"]
-        metadata = {k: example[k] for k in ("title", "abstract")}
-        id2start = {}
-        for entity in example["entities"]:
-            id2start[entity["id"]] = entity["offset"][0]
-        document = DrugprotDocument(
-            text=text,
-            id=doc_id,
-            metadata=metadata,
-        )
-
-        for span in sorted(example["entities"], key=lambda span: span["offset"][0]):
-            labeled_span = LabeledSpan(
-                start=span["offset"][0],
-                end=span["offset"][1],
-                label=span["type"],
-            )
-            document.labeled_spans.append(labeled_span)
-        for relation in sorted(example["relations"], key=lambda relation: relation["id"]):
-            document.binary_relations.append(
-                BinaryRelation(
-                    head=[
-                        span
-                        for span in document.labeled_spans
-                        if span.start == id2start[relation["arg1_id"]]
-                    ][0],
-                    tail=[
-                        span
-                        for span in document.labeled_spans
-                        if span.start == id2start[relation["arg2_id"]]
-                    ][0],
-                    label=relation["type"],
-                )
-            )
-        return document
+    def _generate_document(
+        self,
+        example: Dict[str, Any],
+    ) -> DrugprotDocument | DrugprotBigbioDocument:
+        if self.config.name == "drugprot_source":
+            return example2drugprot(example)
+        elif self.config.name == "drugprot_bigbio_kb":
+            return example2drugprot_bigbio(example)
--- tests/dataset_builders/pie/test_drugprot.py
+++ tests/dataset_builders/pie/test_drugprot.py
@@ -3,8 +3,14 @@ import pytest
 from pytorch_ie.core import Document
 from pytorch_ie.documents import TextDocumentWithLabeledSpansAndBinaryRelations
 
-from dataset_builders.pie.drugprot.drugprot import Drugprot
-from pie_datasets import DatasetDict
+from dataset_builders.pie.drugprot.drugprot import (
+    Drugprot,
+    DrugprotBigbioDocument,
+    DrugprotDocument,
+    example2drugprot,
+    example2drugprot_bigbio,
+)
+from pie_datasets import load_dataset
 from tests.dataset_builders.common import PIE_BASE_PATH
 
 DATASET_NAME = "drugprot"
@@ -49,15 +55,30 @@ def test_hf_example(hf_example, dataset_name):
                     "text": "androstanediol",
                     "offset": [466, 480],
                 },
-                {"id": "17512723_T2", "type": "CHEMICAL", "text": "retinol", "offset": [115, 122]},
-                {"id": "17512723_T3", "type": "CHEMICAL", "text": "retinol", "offset": [9, 16]},
+                {
+                    "id": "17512723_T2",
+                    "type": "CHEMICAL",
+                    "text": "retinol",
+                    "offset": [115, 122],
+                },
+                {
+                    "id": "17512723_T3",
+                    "type": "CHEMICAL",
+                    "text": "retinol",
+                    "offset": [9, 16],
+                },
                 {
                     "id": "17512723_T4",
                     "type": "GENE-Y",
                     "text": "human RDH13",
                     "offset": [219, 230],
                 },
-                {"id": "17512723_T5", "type": "GENE-Y", "text": "RDH12", "offset": [232, 237]},
+                {
+                    "id": "17512723_T5",
+                    "type": "GENE-Y",
+                    "text": "RDH12",
+                    "offset": [232, 237],
+                },
                 {
                     "id": "17512723_T6",
                     "type": "GENE-Y",
@@ -70,7 +91,12 @@ def test_hf_example(hf_example, dataset_name):
                     "text": "human RDH13",
                     "offset": [343, 354],
                 },
-                {"id": "17512723_T8", "type": "GENE-N", "text": "RDHs", "offset": [139, 143]},
+                {
+                    "id": "17512723_T8",
+                    "type": "GENE-N",
+                    "text": "RDHs",
+                    "offset": [139, 143],
+                },
                 {
                     "id": "17512723_T9",
                     "type": "GENE-Y",
@@ -89,7 +115,12 @@ def test_hf_example(hf_example, dataset_name):
                     "text": "human and murine RDH 12",
                     "offset": [191, 214],
                 },
-                {"id": "17512723_T12", "type": "GENE-Y", "text": "RDH12", "offset": [0, 5]},
+                {
+                    "id": "17512723_T12",
+                    "type": "GENE-Y",
+                    "text": "RDH12",
+                    "offset": [0, 5],
+                },
                 {
                     "id": "17512723_T13",
                     "type": "GENE-N",
@@ -238,27 +269,47 @@ def test_hf_example(hf_example, dataset_name):
 
 
 @pytest.fixture(scope="module")
-def document(hf_example, hf_dataset):
-    drugprot = Drugprot()
-    generate_document_kwargs = drugprot._generate_document_kwargs(hf_dataset["train"])
-    document = drugprot._generate_document(example=hf_example, **generate_document_kwargs)
-    return document
+def document(hf_example, dataset_name):
+    if dataset_name == "drugprot_source":
+        return example2drugprot(hf_example)
+    elif dataset_name == "drugprot_bigbio_kb":
+        return example2drugprot_bigbio(hf_example)
 
 
 def test_document(document, dataset_name):
     assert isinstance(document, Document)
     if dataset_name in ("drugprot_source", "drugprot_bigbio_kb"):
+        if dataset_name == "drugprot_source":
+            assert (
+                document.title
+                == "RDH12, a retinol dehydrogenase causing Leber's congenital amaurosis, is also involved in steroid metabolism."
+            )
+            assert (
+                document.abstract
+                == "Three retinol dehydrogenases (RDHs) were tested for steroid converting abilities: human and murine RDH 12 and human RDH13. RDH12 is involved in retinal degeneration in Leber's congenital amaurosis (LCA). We show that murine Rdh12 and human RDH13 do not reveal activity towards the checked steroids, but that human type 12 RDH reduces dihydrotestosterone to androstanediol, and is thus also involved in steroid metabolism. Furthermore, we analyzed both expression and subcellular localization of these enzymes."
+            )
+        elif dataset_name == "drugprot_bigbio_kb":
+            passages = list(document.passages)
+            assert len(passages)
+            assert (
+                str(passages[0])
+                == "RDH12, a retinol dehydrogenase causing Leber's congenital amaurosis, is also involved in steroid metabolism."
+            )
+
         assert (
             document.text
             == "RDH12, a retinol dehydrogenase causing Leber's congenital amaurosis, is also involved in steroid metabolism. Three retinol dehydrogenases (RDHs) were tested for steroid converting abilities: human and murine RDH 12 and human RDH13. RDH12 is involved in retinal degeneration in Leber's congenital amaurosis (LCA). We show that murine Rdh12 and human RDH13 do not reveal activity towards the checked steroids, but that human type 12 RDH reduces dihydrotestosterone to androstanediol, and is thus also involved in steroid metabolism. Furthermore, we analyzed both expression and subcellular localization of these enzymes."
         )
-        entities = list(document.labeled_spans)
+        entities = list(document.entities)
         assert len(entities) == 13
         assert str(entities[0]) == "RDH12"
+        assert document.metadata["entity_id"][0] == "17512723_T12"
         assert str(entities[1]) == "retinol"
+        assert document.metadata["entity_id"][1] == "17512723_T3"
         assert str(entities[-1]) == "androstanediol"
+        assert document.metadata["entity_id"][-1] == "17512723_T1"
 
-        relations = list(document.binary_relations)
+        relations = list(document.relations)
         assert len(relations) == 1
         assert relations[0].label == "PRODUCT-OF"
         assert str(relations[0].head) == "androstanediol"
@@ -268,10 +319,57 @@ def test_document(document, dataset_name):
 
 @pytest.fixture(scope="module")
 def pie_dataset(dataset_name):
-    return DatasetDict.load_dataset(str(PIE_DATASET_PATH), name=dataset_name)
+    return load_dataset(str(PIE_DATASET_PATH), name=dataset_name)
 
 
 def test_pie_dataset(pie_dataset):
     assert set(pie_dataset) == SPLIT_NAMES
     split_sizes = {split_name: len(ds) for split_name, ds in pie_dataset.items()}
     assert split_sizes == SPLIT_SIZES
+
+
+@pytest.fixture(scope="module", params=list(Drugprot.DOCUMENT_CONVERTERS))
+def converter_document_type(request):
+    return request.param
+
+
+@pytest.fixture(scope="module")
+def converted_pie_dataset(pie_dataset, converter_document_type):
+    pie_dataset_converted = pie_dataset.to_document_type(document_type=converter_document_type)
+    return pie_dataset_converted
+
+
+def test_converted_pie_dataset(converted_pie_dataset, converter_document_type):
+    assert set(converted_pie_dataset) == SPLIT_NAMES
+    split_sizes = {split_name: len(ds) for split_name, ds in converted_pie_dataset.items()}
+    assert split_sizes == SPLIT_SIZES
+    for ds in converted_pie_dataset.values():
+        for document in ds:
+            assert isinstance(document, converter_document_type)
+
+
+@pytest.fixture(scope="module")
+def converted_document(converted_pie_dataset):
+    return converted_pie_dataset["train"][0]
+
+
+def test_converted_document(converted_document, converter_document_type):
+    assert isinstance(converted_document, converter_document_type)
+    if converter_document_type == TextDocumentWithLabeledSpansAndBinaryRelations:
+        assert (
+            converted_document.text
+            == "RDH12, a retinol dehydrogenase causing Leber's congenital amaurosis, is also involved in steroid metabolism. Three retinol dehydrogenases (RDHs) were tested for steroid converting abilities: human and murine RDH 12 and human RDH13. RDH12 is involved in retinal degeneration in Leber's congenital amaurosis (LCA). We show that murine Rdh12 and human RDH13 do not reveal activity towards the checked steroids, but that human type 12 RDH reduces dihydrotestosterone to androstanediol, and is thus also involved in steroid metabolism. Furthermore, we analyzed both expression and subcellular localization of these enzymes."
+        )
+        entities = list(converted_document.labeled_spans)
+        assert len(entities) == 13
+        assert str(entities[0]) == "RDH12"
+        assert converted_document.metadata["entity_id"][0] == "17512723_T12"
+        assert str(entities[1]) == "retinol"
+        assert converted_document.metadata["entity_id"][1] == "17512723_T3"
+        assert str(entities[-1]) == "androstanediol"
+        assert converted_document.metadata["entity_id"][-1] == "17512723_T1"
+
+        relations = list(converted_document.binary_relations)
+        assert len(relations) == 1
+        assert relations[0].label == "PRODUCT-OF"
+        assert str(relations[0].head) == "androstanediol"
