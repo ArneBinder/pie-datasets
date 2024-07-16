5f0e904c96b4ec271c3a702a09efbbc38882f0b0
Kai Carhuallanqui <kai_carhuallanqui@yahoo.com>
2024-07-16 11:31:06 +0200

    Adjusted test_hf_example

--- tests/dataset_builders/pie/chemprot/test_chemprot.py
+++ tests/dataset_builders/pie/chemprot/test_chemprot.py
@@ -15,7 +15,7 @@ SPLIT_SIZES = {"sample": 50, "test": 800, "train": 1020, "validation": 612}
 
 @pytest.fixture(scope="module", params=[config.name for config in Chemprot.BUILDER_CONFIGS])
 def dataset_variant(request) -> str:
-    return "chemprot_shared_task_eval_source"
+    return request.param
 
 
 @pytest.fixture(scope="module")
@@ -37,7 +37,10 @@ def hf_example(hf_dataset):
 
 def test_hf_example(hf_example, dataset_variant):
     assert hf_example is not None
-    if dataset_variant == "chemprot_full_source":
+    if (
+        dataset_variant == "chemprot_full_source"
+        or dataset_variant == "chemprot_shared_task_eval_source"
+    ):
         assert hf_example == {
             "pmid": "16357751",
             "text": "Selective costimulation modulators: a novel approach for the treatment of rheumatoid arthritis.\nT cells have a central role in the orchestration of the immune pathways that contribute to the inflammation and joint destruction characteristic of rheumatoid arthritis (RA). The requirement for a dual signal for T-cell activation and the construction of a fusion protein that prevents engagement of the costimulatory molecules required for this activation has led to a new approach to RA therapy. This approach is mechanistically distinct from other currently used therapies; it targets events early rather than late in the immune cascade, and it results in immunomodulation rather than complete immunosuppression. The fusion protein abatacept is a selective costimulation modulator that avidly binds to the CD80/CD86 ligands on an antigen-presenting cell, resulting in the inability of these ligands to engage the CD28 receptor on the T cell. Abatacept dose-dependently reduces T-cell proliferation, serum concentrations of acute-phase reactants, and other markers of inflammation, including the production of rheumatoid factor by B cells. Recent studies have provided consistent evidence that treatment with abatacept results in a rapid onset of efficacy that is maintained over the course of treatment in patients with inadequate response to methotrexate and anti-tumor necrosis factor therapies. This efficacy includes patient-centered outcomes and radiographic measurement of disease progression. Abatacept has also demonstrated a very favorable safety profile to date. This article reviews the rationale for this therapeutic approach and highlights some of the recent studies that demonstrate the benefits obtained by using abatacept. This clinical experience indicates that abatacept is a significant addition to the therapeutic armamentarium for the management of patients with RA.",
@@ -103,18 +106,6 @@ def test_hf_example(hf_example, dataset_variant):
             "events": [],
             "coreferences": [],
             "relations": [],
-        }
-    elif dataset_variant == "chemprot_shared_task_eval_source":
-        assert hf_example == {
-            "pmid": "16357751",
-            "text": "Selective costimulation modulators: a novel approach for the treatment of rheumatoid arthritis.\nT cells have a central role in the orchestration of the immune pathways that contribute to the inflammation and joint destruction characteristic of rheumatoid arthritis (RA). The requirement for a dual signal for T-cell activation and the construction of a fusion protein that prevents engagement of the costimulatory molecules required for this activation has led to a new approach to RA therapy. This approach is mechanistically distinct from other currently used therapies; it targets events early rather than late in the immune cascade, and it results in immunomodulation rather than complete immunosuppression. The fusion protein abatacept is a selective costimulation modulator that avidly binds to the CD80/CD86 ligands on an antigen-presenting cell, resulting in the inability of these ligands to engage the CD28 receptor on the T cell. Abatacept dose-dependently reduces T-cell proliferation, serum concentrations of acute-phase reactants, and other markers of inflammation, including the production of rheumatoid factor by B cells. Recent studies have provided consistent evidence that treatment with abatacept results in a rapid onset of efficacy that is maintained over the course of treatment in patients with inadequate response to methotrexate and anti-tumor necrosis factor therapies. This efficacy includes patient-centered outcomes and radiographic measurement of disease progression. Abatacept has also demonstrated a very favorable safety profile to date. This article reviews the rationale for this therapeutic approach and highlights some of the recent studies that demonstrate the benefits obtained by using abatacept. This clinical experience indicates that abatacept is a significant addition to the therapeutic armamentarium for the management of patients with RA.",
-            "entities": {
-                "id": ["T1", "T2", "T3", "T4", "T5"],
-                "type": ["CHEMICAL", "GENE-N", "GENE-Y", "GENE-Y", "GENE-N"],
-                "text": ["methotrexate", "tumor necrosis factor", "CD80", "CD86", "CD28 receptor"],
-                "offsets": [[1342, 1354], [1364, 1385], [805, 809], [810, 814], [912, 925]],
-            },
-            "relations": {"type": [], "arg1": [], "arg2": []},
         }
     else:
         raise ValueError(f"Unknown dataset variant: {dataset_variant}")
