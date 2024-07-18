e641562ed7f89554698f0e1d461b63010b8458b5
Kai Carhuallanqui <kai_carhuallanqui@yahoo.com>
2024-07-18 15:19:15 +0200

    Test example to document

--- tests/dataset_builders/pie/tbga/test_tbga.py
+++ tests/dataset_builders/pie/tbga/test_tbga.py
@@ -1,6 +1,7 @@
 import pytest
 from datasets import disable_caching, load_dataset
 
+from dataset_builders.pie.tbga.tbga import example_to_document
 from tests.dataset_builders.common import PIE_BASE_PATH
 
 DATASET_NAME = "tbga"
@@ -63,3 +64,21 @@ def test_hf_example(hf_example, split):
         }
     else:
         raise ValueError(f"Invalid split: {split}")
+
+
+def test_example_to_document(hf_example, split):
+    doc = example_to_document(hf_example)
+    if split == "test":
+        assert doc.entities.resolve() == [(50940, 'PDE11A', ''), ('C0006826', 'Malignant Neoplasms', '')]
+        assert doc.relations.resolve() == [('NA', ((50940, 'PDE11A', ''), ('C0006826', 'Malignant Neoplasms', '')))]
+
+    elif split == "train":
+        assert doc.entities.resolve() == [(6347, 'CCL2', 'monocyte chemoattractant protein'), ('C0231221', 'Asymptomatic', '')]
+        assert doc.relations.resolve() == [('NA', ((6347, 'CCL2', 'monocyte chemoattractant protein'), ('C0231221', 'Asymptomatic', '')))]
+
+    elif split == "validation":
+        assert doc.entities.resolve() == [(51726, 'DNAJB11', ''), ('C0000768', 'Congenital Abnormality', '')]
+        assert doc.relations.resolve() == [('NA', ((51726, 'DNAJB11', ''), ('C0000768', 'Congenital Abnormality', '')))]
+
+    else:
+        raise ValueError(f"Unknown split variant: {split}")
