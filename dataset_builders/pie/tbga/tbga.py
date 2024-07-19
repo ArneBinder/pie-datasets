a3f21e61c4d73a268be2f56ef2da188e9c594f0f
Kai Carhuallanqui <kai_carhuallanqui@yahoo.com>
2024-07-19 10:53:01 +0200

    Document Converters

--- dataset_builders/pie/tbga/tbga.py
+++ dataset_builders/pie/tbga/tbga.py
@@ -3,8 +3,11 @@ from typing import Any
 
 import datasets
 from pytorch_ie import AnnotationLayer, annotation_field
-from pytorch_ie.annotations import BinaryRelation, Span
-from pytorch_ie.documents import TextBasedDocument
+from pytorch_ie.annotations import BinaryRelation, LabeledSpan, Span
+from pytorch_ie.documents import (
+    TextBasedDocument,
+    TextDocumentWithLabeledSpansAndBinaryRelations,
+)
 
 from pie_datasets import ArrowBasedBuilder, GeneratorBasedBuilder
 
@@ -31,13 +34,13 @@ def example_to_document(example) -> TbgaDocument:
         id=str(example["h"]["id"]),
         name=example["h"]["name"],
         start=example["h"]["pos"][0],
-        end=example["h"]["pos"][1],
+        end=example["h"]["pos"][0] + example["h"]["pos"][1],  # end is start + length
     )
     tail = SpanWithIdAndName(
         id=example["t"]["id"],
         name=example["t"]["name"],
         start=example["t"]["pos"][0],
-        end=example["t"]["pos"][1],
+        end=example["t"]["pos"][0] + example["t"]["pos"][1],  # end is start + length
     )
     document.entities.extend([head, tail])
 
@@ -52,9 +55,45 @@ def document_to_example(document):
     return {
         "text": document.text,
         "relation": document.relations[0].label,
-        "h": {"id": int(head.id), "name": head.name, "pos": [head.start, head.end]},
-        "t": {"id": tail.id, "name": tail.name, "pos": [tail.start, tail.end]},
+        "h": {"id": int(head.id), "name": head.name, "pos": [head.start, head.end - head.start]},
+        "t": {"id": tail.id, "name": tail.name, "pos": [tail.start, tail.end - tail.start]},
     }
+
+
+def convert_to_text_document_with_labeled_spans_and_binary_relations(
+    document: TbgaDocument,
+) -> TextDocumentWithLabeledSpansAndBinaryRelations:
+    text_document = TextDocumentWithLabeledSpansAndBinaryRelations(text=document.text)
+    old2new_spans = {}
+    ids = []
+    names = []
+
+    for entity in document.entities:  # in our case two entities (head and tail)
+        # create LabeledSpan and append
+        labeled_span = LabeledSpan(start=entity.start, end=entity.end, label="ENTITY")
+        text_document.labeled_spans.append(labeled_span)
+
+        # Map the original entity to the new labeled span
+        old2new_spans[entity] = labeled_span
+
+        ids.append(entity.id)
+        names.append(entity.name)
+
+    if len(document.relations) != 1:  # one relation between two entities
+        raise ValueError(f"Expected exactly one relation, got {len(document.relations)}")
+    old_rel = document.relations[0]
+
+    # create BinaryRelation and append
+    rel = BinaryRelation(
+        head=old2new_spans[old_rel.head],
+        tail=old2new_spans[old_rel.tail],
+        label=old_rel.label,
+    )
+    text_document.binary_relations.append(rel)
+    text_document.metadata["entity_ids"] = ids
+    text_document.metadata["entity_names"] = names
+
+    return text_document
 
 
 class Tbga(ArrowBasedBuilder):
@@ -68,6 +107,10 @@ class Tbga(ArrowBasedBuilder):
             description="TBGA dataset",
         )
     ]
+
+    DOCUMENT_CONVERTERS = {
+        TextDocumentWithLabeledSpansAndBinaryRelations: convert_to_text_document_with_labeled_spans_and_binary_relations
+    }
 
     def _generate_document(self, example, **kwargs):
         return example_to_document(example)
