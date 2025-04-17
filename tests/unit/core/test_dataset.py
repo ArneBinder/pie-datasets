ec193e2a4651d57936048c6bc6a306a9b8615dc3
Arne Binder <arne.binder@dfki.de>
2025-04-17 14:52:37 +0200

    minor

--- tests/unit/core/test_dataset.py
+++ tests/unit/core/test_dataset.py
@@ -4,7 +4,6 @@ from pathlib import Path
 from typing import Union
 
 import pytest
-import torch
 from pytorch_ie import Document
 from pytorch_ie.annotations import BinaryRelation, Label, LabeledSpan, Span
 from pytorch_ie.core import AnnotationList, annotation_field
